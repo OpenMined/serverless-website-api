05e8d67833c416353c910aaa7be88a9ef5541fad
cereallarceny <me@patrickcason.com>
2019-02-17 16:29:09 -0300

    Updating the route

--- ghost/index.js
+++ ghost/index.js
@@ -2,11 +2,9 @@ export default async () => {
   const params =
     'limit=10&fields=id,title,slug,feature_image,custom_excerpt,url,published_at';
 
-  const getPosts = (subdomain, token) =>
+  const getPosts = token =>
     fetch(
-      'https://' +
-        subdomain +
-        '.ghost.io/ghost/api/v0.1/posts? ' +
+      'https://blog.openmined.org/ghost/api/v0.1/posts? ' +
         params +
         '&client_id=ghost-frontend&client_secret=' +
         token
@@ -23,10 +21,7 @@ export default async () => {
         throw new Error(error);
       });
 
-  const blog = await getPosts(
-    'openmined-blog',
-    process.env.OPENMINED_BLOG_TOKEN
-  );
+  const blog = await getPosts(process.env.OPENMINED_BLOG_TOKEN);
 
   return {
     blog: blog.posts
